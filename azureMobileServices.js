1421ebe91c8239f3b2bffe77830e6288f4079811
Rob Heckart <rob.heckart@gmail.com>
2013-07-06 21:27:51 -0400

    Major refactor to use promises.

--- azureMobileServices.js
+++ azureMobileServices.js
@@ -10,17 +10,12 @@ angular.module('App')
 
   // Login to Azure method. Takes a string (socialMediaService) with the social media service being used (Facebook, Twitter, etc.)
   // Returns a boolean value indicating success or failure
-  azureMobileClient.login = function(callback, socialMediaService) {
-
-    azureMobileClient.azureMSC.login(socialMediaService).then(function(user) {
-      azureMobileClient.isLoggedIn = user != null;
-      $cookieStore.put("azureUser", user);
-      callback(azureMobileClient.isLoggedIn);
-    }
-    , function(error){
-      azureMobileClient.azureError = error;
-      callback(false);
-    });
+  azureMobileClient.login = function(socialMediaService) {
+    return azureMobileClient.azureMSC.login(socialMediaService).then(
+      function(user) {
+        $cookieStore.put("azureUser", user);
+      }
+      );
   };
 
   // Logs out from the Azure service
@@ -31,54 +26,23 @@ angular.module('App')
   };
 
   // Get all data using the read method of the Azure service. Take a string (tableName) indicating the table to get data from
-  azureMobileClient.getAllData = function(callback, tableName) {
-
-    azureMobileClient.getUser();
-
-    var dataTable = azureMobileClient.azureMSC.getTable(tableName);
-
-    dataTable.read().then(function(data) {
-      callback(data);
-    });
+  azureMobileClient.getAllData = function(tableName) {
+    return azureMobileClient.prepService(tableName).read();
   };
 
   // Adds a data row (data) to the specified table (tableName). 
   // If success, returns the new object that was created.
   // If failure, returns the error text on failure.
-  azureMobileClient.addData = function(callback, tableName, data) {
-    azureMobileClient.getUser();
-    var dataTable = azureMobileClient.azureMSC.getTable(tableName);
-    dataTable.insert(data).then(function(success) {
-      callback(success);
-    }
-    , function(error) {
-      azureMobileClient.azureError = error.request.responseText;
-      callback(error.request.responseText);
-    });
+  azureMobileClient.addData = function(tableName, data) {
+    return azureMobileClient.prepService(tableName).insert(data);
   };
 
-  azureMobileClient.updateData = function(callback, tableName, data) {
-    azureMobileClient.getUser();
-    var dataTable = azureMobileClient.azureMSC.getTable(tableName);
-    dataTable.update(data).then(function(success) {
-      callback(success);
-    }
-    , function(error) {
-      azureMobileClient.azureError = error.request.responseText;
-      callback(error.request.responseText);
-    });
+  azureMobileClient.updateData = function(tableName, data) {
+    return azureMobileClient.prepService(tableName).update(data);
   };
 
-  azureMobileClient.deleteData = function(callback, tableName, data) {
-    azureMobileClient.getUser();
-    var dataTable = azureMobileClient.azureMSC.getTable(tableName);
-    dataTable.del(data).then(function(success) {
-      callback(true);
-    }
-    , function(error) {
-      azureMobileClient.azureError = error.request.responseText;
-      callback(false);
-    });
+  azureMobileClient.deleteData = function(tableName, data) {
+    return azureMobileClient.prepService(tableName).del(data);
   };
 
   // Gets the currentUser back from the cookieStore if the currentUser object is null
@@ -87,6 +51,12 @@ angular.module('App')
     {
       azureMobileClient.azureMSC.currentUser = $cookieStore.get("azureUser");
     }
+  };
+
+  // Sets the correct user and gets the appropriate table from Azure. Used in each call above to simplify methods
+  azureMobileClient.prepService = function(tableName) {
+    azureMobileClient.getUser();
+    return azureMobileClient.azureMSC.getTable(tableName);
   };
 
   return azureMobileClient;
